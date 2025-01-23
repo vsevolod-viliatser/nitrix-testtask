fe97cadc35e9c7247922a7786fb505d89f0d8d3e
vsevolod-viliatser <sevavilyatser@gmail.com>
2025-01-24 00:25:11 +0200

    cors

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,16 @@ const apartmentRoutes = require("./routes/apartmentRoutes");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',  // или укажите точный домен, если хотите ограничить доступ
+  }));
 app.use(bodyParser.json());
 app.use("/api/apartments", apartmentRoutes);
 // Раздача статических файлов из папки frontend/dist
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 const cors = require('cors');
 
-app.use(cors({
-  origin: '*',  // или укажите точный домен, если хотите ограничить доступ
-}));
+
 
 // Обработка всех остальных маршрутов (для React)
 app.get('*', (req, res) => {
