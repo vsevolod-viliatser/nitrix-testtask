4314a5813c81998997db77771bfa9e0d7d82abf7
vsevolod-viliatser <sevavilyatser@gmail.com>
2025-01-23 22:46:55 +0200

    commit before render

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,19 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDB = require("./db");
 const apartmentRoutes = require("./routes/apartmentRoutes");
-
+const path = require("path")
 const app = express();
 connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/apartments", apartmentRoutes);
-app.use(express.static(path.join(__dirname, 'dist')));
+// Раздача статических файлов
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// Обработка остальных маршрутов для React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
